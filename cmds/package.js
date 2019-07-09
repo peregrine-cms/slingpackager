cddcb91ca327815093c658d6e7702627191e5ed4
Henry Saginor <hsaginor@gmail.com>
2019-07-09 09:59:07 -0700

    Implementing package command.

--- cmds/package.js
+++ cmds/package.js
@@ -1,5 +1,55 @@
-exports.command = 'package <folder>'
+const archiver = require('archiver')
+const fs = require('fs')
+const path = require('path')
+
+exports.command = 'package <folder> <package>'
 exports.desc = 'create a package'
-exports.handler = function (argv) {
-  console.log('package folder', argv.folder)
+exports.handler = (argv) => {
+  if(isValid(argv.folder)) {
+    archive(argv);
+  }
+}
+
+function archive(argv) {
+  var packagePath = argv.package;
+  if(!path.isAbsolute(packagePath)) {
+    packagePath = path.join(__dirname, packagePath);
+  }
+
+  console.log('package folder', argv.folder, 'as', packagePath);
+  var output = fs.createWriteStream(packagePath);
+  var archive = archiver('zip');
+
+  archive.on('error', (err) => { throw err });
+
+  archive.pipe(output);
+
+  var jcrRoot = path.join(argv.folder, 'jcr_root');
+  archive.directory(jcrRoot, 'jcr_root', { name: 'jcr_root' });
+
+  var metainf = path.join(argv.folder, 'META-INF');
+  archive.directory(metainf, 'META-INF', { name: 'META-INF' });
+
+  archive.finalize();
+}
+
+function isValid(dir) {
+  if(!fs.existsSync(dir) || !fs.statSync(dir).isDirectory()) {
+    console.log(dir,"does not exist or is not a folder.");
+    return false;
+  }
+
+  var jcrRoot = path.join(dir, 'jcr_root');
+  if(!fs.existsSync(jcrRoot) || !fs.statSync(jcrRoot).isDirectory()) {
+    console.log(jcrRoot,"does not exist or is not a folder.");
+    return false;
+  }
+
+  var metainf = path.join(dir, 'META-INF');
+  if(!fs.existsSync(metainf) || !fs.statSync(metainf).isDirectory()) {
+    console.log(metainf,"does not exist or is not a folder.");
+    return false;
+  }
+
+  return true;
 }
